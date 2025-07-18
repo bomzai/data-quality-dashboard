b3cff788d4e4e054393ae3b1ae55c7d05a26453c
Anes ABBAD <user.name>
2025-07-18 15:11:17 +0200

    new version of the dashboard

--- main.py
+++ main.py
@@ -14,20 +14,57 @@ st.write("""
 Ceci est **un exemple** de visualisations pour illustrer la qualité des données.
 """)
 
+SCHEMA = "DEV_RESULTS.SODA_SCAN_RESULTS"
+
 # --- Chargement des données depuis Snowflake ---
 session = get_active_session()
-snowpark_df = session.table("public.DQ_DATA")
+snowpark_df = session.table(SCHEMA)
 df = snowpark_df.to_pandas()
-df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+
+tz = df['SCAN_TIMESTAMP'].dt.tz
+df["SCAN_TIMESTAMP"] = pd.to_datetime(df["SCAN_TIMESTAMP"]).dt.tz_convert('Europe/Paris')
+min_datetime = df["SCAN_TIMESTAMP"].min().normalize()
+max_datetime = df["SCAN_TIMESTAMP"].max().normalize()
+min_date = min_datetime.date()
+max_date = max_datetime.date()
+df["Indicateur"] = df['CHECK_NAME'].str.split('-').str[0]
+df["Test_booleen"] = 1
+df["Référentiel"] = "Produit"
+df["Résultat"] = df["VALUE"]
+
+df["Score"] = (df["Résultat"] == 0).mean() * 100
+
 
 # --- Score global pondéré ---
 # Calcul sur la dernière date disponible
-timestamp_latest = df["Timestamp"].max().normalize()
-df_latest = df[df["Timestamp"].dt.normalize() == timestamp_latest]
+timestamp_latest = df["SCAN_TIMESTAMP"].max().normalize()
+df_latest = df[df["SCAN_TIMESTAMP"].dt.normalize() == timestamp_latest]
 
 # Liste des indicateurs et poids modifiables
-all_indicators = sorted(df_latest["Indicateur de qualité"].unique())
+all_indicators = sorted(df_latest["Indicateur"].unique())
 poids_indicateurs = {}
+
+def interpret_score(data):
+    good_data_quality = data.loc[data["Résultat"] == 0]
+    good_data_quality_columns = good_data_quality['COLUMN_NAME'].unique()
+    bad_data_quality_columns = list(set(data['COLUMN_NAME'].unique() ) - set(good_data_quality_columns))
+    result = (len(good_data_quality_columns) / len(data['COLUMN_NAME'].unique())) * 100
+    return result
+
+
+# Calcul des scores par granularité
+def calculer_score_temporel(df_group):
+    total, poids_total = 0, 0
+    for indic in all_indicators:
+        df_ind = df_group[df_group["Indicateur"] == indic]
+        if df_ind.empty:
+            continue
+        poids = poids_indicateurs.get(indic, 1.0)
+        #score = pd.to_numeric(df_ind["Résultat"], errors="coerce").mean()
+        score = interpret_score(df_ind)
+        total += poids * score
+        poids_total += poids
+    return total / poids_total if poids_total > 0 else None
 
 # Ajustement des pondérations dans une zone masquée par défaut
 with st.sidebar:
@@ -39,21 +76,8 @@ with st.sidebar:
     st.subheader("Seuil pour l'évolutiuon Score global dans le temps : ")
     seuil = st.slider("Seuil du score global (%)", min_value=0, max_value=100, value=50)
 
-# Calcul du score pondéré
-total_pondere = 0
-somme_poids = 0
-for indic in all_indicators:
-    df_indic = df_latest[df_latest["Indicateur de qualité"] == indic]
-    if df_indic.empty:
-        continue
-    poids = poids_indicateurs[indic]
-    if df_indic["Test_booleen"].iloc[0]:
-        score = df_indic["Résultat"].astype(int).mean() * 100
-    else:
-        score = pd.to_numeric(df_indic["Résultat"], errors="coerce").mean()
-    total_pondere += poids * score
-    somme_poids += poids
-score_final = total_pondere / somme_poids if somme_poids > 0 else 0
+
+score_final = calculer_score_temporel(df)
 
 # Détermination de la couleur
 if score_final < 50:
@@ -63,22 +87,29 @@ elif score_final < 70:
 else:
     color = "#2ecc71"
 
+
 # --- Filtres sur la même ligne ---
 col1, col2 = st.columns([1, 1])
 with col1:
-    min_date = df["Timestamp"].min().date()
-    max_date = df["Timestamp"].max().date()
     date_range = st.date_input(
-        "Intervalle de temps :",
+        "Intervalle de temps",
         value=(min_date, max_date),
         min_value=min_date,
         max_value=max_date
     )
+    if isinstance(date_range, tuple) and len(date_range) == 2:
+        start_date, end_date = date_range
+        # 5. Convertir en datetime avec timezone
+        start_datetime = pd.Timestamp(start_date).tz_localize(tz)
+        end_datetime = pd.Timestamp(end_date).tz_localize(tz) + pd.Timedelta(days=1) - pd.Timedelta(seconds=1)
+    else:
+        st.warning("Veuillez sélectionner une plage de dates.")
+
 with col2:
     # Choix de la granularité temporelle
     granularite = st.radio("Granularité temporelle :", ["Jour", "Semaine", "Mois", "Trimestre"], horizontal=True)
 
-def appliquer_granularite(df, colonne="Timestamp"):
+def appliquer_granularite(df, colonne="SCAN_TIMESTAMP"):
     if granularite == "Jour":
         return df[colonne].dt.to_period("D").dt.to_timestamp()
     elif granularite == "Semaine":
@@ -89,42 +120,13 @@ def appliquer_granularite(df, colonne="Timestamp"):
         trimestre = df[colonne].dt.to_period("Q").dt.start_time
         return pd.to_datetime(trimestre)
 
-# Gestion de la plage de dates robuste même si une seule date est sélectionnée
-if isinstance(date_range, tuple):
-    if len(date_range) == 2:
-        start_date, end_date = date_range
-    elif len(date_range) == 1:
-        start_date = date_range[0]
-        end_date = max_date
-    else:
-        start_date = min_date
-        end_date = max_date
-else:
-    start_date = date_range
-    end_date = max_date
 
 # --- Application du filtre de temps global au DataFrame ---
-df = df[(df["Timestamp"].dt.date >= start_date) & (df["Timestamp"].dt.date <= end_date)]
+df = df[(df["SCAN_TIMESTAMP"].dt.date >= min_date) & (df["SCAN_TIMESTAMP"].dt.date <= max_date)]
 
 # --- Évolution du score global dans le temps ---
-df_filtered = df[df["Indicateur de qualité"].isin(all_indicators)]
+df_filtered = df[df["Indicateur"].isin(all_indicators)]
 df_filtered["Granularité"] = appliquer_granularite(df_filtered)
-
-# Calcul des scores par granularité
-def calculer_score_temporel(df_group):
-    total, poids_total = 0, 0
-    for indic in all_indicators:
-        df_ind = df_group[df_group["Indicateur de qualité"] == indic]
-        if df_ind.empty:
-            continue
-        poids = poids_indicateurs.get(indic, 1.0)
-        if df_ind["Test_booleen"].iloc[0]:
-            score = df_ind["Résultat"].astype(int).mean() * 100
-        else:
-            score = pd.to_numeric(df_ind["Résultat"], errors="coerce").mean()
-        total += poids * score
-        poids_total += poids
-    return total / poids_total if poids_total > 0 else None
 
 df_scores = df_filtered.groupby("Granularité").apply(calculer_score_temporel).reset_index(name="Score global")
 
@@ -173,10 +175,7 @@ seuil_line = alt.Chart(pd.DataFrame({'y': [seuil]})).mark_rule(
 st.altair_chart((line_chart+seuil_line), use_container_width=True)
 
 # Filtrage du DataFrame selon l’intervalle de dates
-df_with_interval = df[
-    (df['Timestamp'] >= pd.to_datetime(start_date)) & 
-    (df['Timestamp'] <= pd.to_datetime(end_date))
-]
+df_with_interval = df[(df["SCAN_TIMESTAMP"] >= start_datetime) & (df["SCAN_TIMESTAMP"] <= end_datetime)]
 
 # Filtrer les tests booléens uniquement (puisqu'on s'intéresse à OK/KO)
 df_bool_ko = df_with_interval[
@@ -187,7 +186,7 @@ df_bool_ko = df_with_interval[
 df_ko_only = df_bool_ko[df_bool_ko['Résultat'] == 0]
 
 # Compter les KO par Référentiel / Table / Champ
-df_ko_counts = df_ko_only.groupby(['Référentiel', 'Table ref', 'Champ']).size().reset_index(name='Tests KO')
+df_ko_counts = df_ko_only.groupby(['Référentiel', 'TABLE_NAME', 'COLUMN_NAME']).size().reset_index(name='Tests KO')
 
 # Si aucun KO, ajouter une ligne factice pour éviter une erreur Plotly
 if df_ko_counts.empty:
@@ -199,19 +198,17 @@ if df_ko_counts.empty:
     })
 
 # Treemap Plotly
-fig = px.treemap(
-    df_ko_counts,
-    path=['Référentiel', 'Table ref', 'Champ'],
-    values='Tests KO',
-    color='Tests KO',
-    color_continuous_scale='Reds',
-)
+#fig = px.treemap(
+#    df_ko_counts,
+#    path=['Référentiel', 'TABLE_NAME', 'COLUMN_NAME'],
+#    values='Tests KO',
+#    color='Tests KO',
+#    color_continuous_scale='Reds',
+#)
+#st.plotly_chart(fig, use_container_width=True)
 
-st.plotly_chart(fig, use_container_width=True)
+#Visualisations Antoine pour remplacer les treemap
 
-"""
-Visualisations Antoine pour remplacer les treemap
-"""
 # 1. Sélection du référentiel
 referentiels = df['Référentiel'].dropna().unique()
 selected_referentiel = st.selectbox("Sélectionnez le référentiel", referentiels)
@@ -220,21 +217,24 @@ selected_referentiel = st.selectbox("Sélectionnez le référentiel", referentie
 df_ref = df_with_interval[df_with_interval['Référentiel'] == selected_referentiel]
 
 
-
-indicateurs = df_ref['Indicateur de qualité'].unique()
-
 with st.container():
     col1, col2 = st.columns([1, 2])
+    indicateurs = df_ref['Indicateur'].unique()
+    scores = {}
+    global_score = 0
+    for i in indicateurs:
+        data = df_ref.loc[df_ref["Indicateur"] == i]
+        score = interpret_score(data)
+        scores[i] = score
+        global_score += score
+    global_score = global_score / len(indicateurs)
     
     with col1:
         # 1.1 Donut Global (tous indicateurs confondus)
         st.markdown("### QDD global")
-        
-        total_tests_global = len(df_ref)
-        tests_ok_global = (df_ref['Résultat'] == 1).sum()
-        tests_ko_global = (df_ref['Résultat'] == 0).sum()
-        
-        taux_ok_global = (tests_ok_global / total_tests_global) * 100 if total_tests_global > 0 else 0
+ 
+        tests_ko_global = 100 - global_score       
+        taux_ok_global = global_score
         taux_ko_global = 100 - taux_ok_global
         
         fig_global = go.Figure(data=[go.Pie(
@@ -270,12 +270,8 @@ with st.container():
             for j in range(3):
                 if i + j < len(indicateurs):
                     indicateur = indicateurs[i + j]
-                    df_indic = df_ref[df_ref['Indicateur de qualité'] == indicateur]
-                    total_tests = len(df_indic)
-                    tests_ok = (df_indic['Résultat'] == 1).sum()
-                    tests_ko = (df_indic['Résultat'] == 0).sum()
-                    
-                    taux_ok = (tests_ok / total_tests) * 100 if total_tests > 0 else 0
+                    df_indic = df_ref[df_ref['Indicateur'] == indicateur]
+                    taux_ok = scores[indicateur]
                     taux_ko = 100 - taux_ok
                 
                     fig = go.Figure(data=[go.Pie(
@@ -333,29 +329,47 @@ def gradient_color(val):
 # Filtrage des données sur le référentiel choisi
 filtered_df = df_with_interval[df_with_interval["Référentiel"] == selected_referentiel]
 # Calcul du taux de réussite par Table ref et Indicateur de qualité
-pivot_table = filtered_df.groupby(["Table ref", "Indicateur de qualité"])["Résultat"].mean().unstack()
-pivot_table[["Cohérence","Intégrité","Pertinence","Traçabilité"]] = pivot_table[["Cohérence","Intégrité","Pertinence","Traçabilité"]] * 100
+#pivot_table = filtered_df.groupby(["TABLE_NAME", "Indicateur"])["Résultat"].mean().unstack()
+pivot_table = (
+    filtered_df
+    .groupby(["TABLE_NAME", "Indicateur"])
+    .apply(interpret_score)
+    .unstack()
+)
+
 # Calcul de la Globalité : taux global de tests OK==True pour chaque Table ref
-globalite_table = filtered_df.groupby("Table ref")["Résultat"].mean()
+#globalite_table = filtered_df.groupby("TABLE_NAME")["Résultat"].mean()
+globalite_table = (
+    filtered_df
+    .groupby(["TABLE_NAME", "Indicateur"])
+    .apply(interpret_score)
+)
 # Ajout de la colonne Globalité dans le pivot
 pivot_table["Globalité"] = globalite_table
-# Affichage
 st.subheader(f"Taux de réussite par table pour le référentiel : {selected_referentiel}")
-#st.dataframe(pivot_table.round(2), use_container_width=True)
 styled_df = pivot_table.round(2).style.format("{:.2f}%").applymap(gradient_color)
-# Affichage Streamlit
 st.dataframe(styled_df, use_container_width=True)
 
 # 3e visualisation : DataFrame Champ vs Indicateurs pour une Table sélectionnée + Globalité réelle (OK==True)
 # Filtre sur la table
-selected_table = st.selectbox("Sélectionner une table", filtered_df["Table ref"].unique())
+selected_table = st.selectbox("Sélectionner une table", filtered_df["TABLE_NAME"].unique())
 # Filtrage sur la table sélectionnée
-filtered_table_df = filtered_df[filtered_df["Table ref"] == selected_table]
+filtered_table_df = filtered_df[filtered_df["TABLE_NAME"] == selected_table]
 # Calcul du taux de réussite par Champ et Indicateur de qualité
-pivot_champ = filtered_table_df.groupby(["Champ", "Indicateur de qualité"])["Résultat"].mean().unstack()
-pivot_champ[["Cohérence","Intégrité","Pertinence","Traçabilité"]] = pivot_champ[["Cohérence","Intégrité","Pertinence","Traçabilité"]] * 100
+#pivot_champ = filtered_table_df.groupby(["COLUMN_NAME", "Indicateur"])["Résultat"].mean().unstack()
+pivot_champ = (
+    filtered_table_df
+    .groupby(["COLUMN_NAME", "Indicateur"])
+    .apply(interpret_score)
+    .unstack()
+)
 # Calcul de la Globalité : taux global de tests OK==True pour chaque Champ
-globalite_champ = filtered_table_df.groupby("Champ")["Résultat"].mean()
+#globalite_champ = filtered_table_df.groupby("COLUMN_NAME")["Résultat"].mean()
+globalite_champ = (
+    filtered_table_df
+    .groupby(["COLUMN_NAME", "Indicateur"])
+    .apply(interpret_score)
+)
 # Ajout de la colonne Globalité dans le pivot
 pivot_champ["Globalité"] = globalite_champ
 # Affichage
@@ -367,27 +381,27 @@ st.dataframe(styled_df, use_container_width=True)
 col1, col2 = st.columns([1, 1])
 with col1:
     # Création du filtre Champ avec affichage "Référentiel - Table ref - Champ"
-    df['Champ Full'] = df['Champ'] + ' - ' + df['Table ref'] + ' - ' +  df['Référentiel']
+    df['Champ Full'] = df['COLUMN_NAME'] + ' - ' + df['TABLE_NAME'] + ' - ' +  df['Référentiel']
     unique_champs = df['Champ Full'].unique()
     champ = st.selectbox("Sélectionner un champ", unique_champs)
     
     # Extraire les composantes pour filtrer ensuite
     champ_selectionne, selected_table, selected_ref = champ.split(' - ')
 with col2:
-    indicateurs_disponibles = df["Indicateur de qualité"].unique().tolist()
+    indicateurs_disponibles = df["Indicateur"].unique().tolist()
     indicator_selected = st.radio(
         "Choisissez un indicateur :",
         indicateurs_disponibles,
         horizontal=True
     )
 # --- Application du filtre de temps global au DataFrame ---
-df = df[(df["Timestamp"].dt.date >= start_date) & (df["Timestamp"].dt.date <= end_date)]
+df = df[(df["SCAN_TIMESTAMP"].dt.date >= min_date) & (df["SCAN_TIMESTAMP"].dt.date <= max_date)]
 
 # --- Scatter / Line selon indicateur ---
 st.subheader("Évolution par indicateur")
 df_filtered = df[
-    (df["Champ"] == champ_selectionne) &
-    (df["Indicateur de qualité"] == indicator_selected)
+    (df["COLUMN_NAME"] == champ_selectionne) &
+    (df["Indicateur"] == indicator_selected)
 ]
 if df_filtered.empty:
     st.warning("Aucune donnée disponible pour l’indicateur sélectionné.")
@@ -395,8 +409,8 @@ else:
     df_filtered["Date"] = appliquer_granularite(df_filtered)
     df_filtered["Résultat"] = pd.to_numeric(df_filtered["Résultat"], errors="coerce")
     df_filtered = df_filtered.groupby("Date", as_index=False)["Résultat"].mean()
-    latest_value = df_filtered[df_filtered["Date"].dt.date == end_date]["Résultat"].mean()
-    st.markdown(f"**Valeur de l’indicateur le {end_date.strftime('%d-%m-%Y')} :** {latest_value:.2f}")
+    latest_value = df_filtered[df_filtered["Date"].dt.date == max_date]["Résultat"].mean()
+    st.markdown(f"**Valeur de l’indicateur le {max_date.strftime('%d-%m-%Y')} :** {latest_value:.2f}")
     line = alt.Chart(df_filtered).mark_line(point=True).encode(
         x=alt.X("Date:T", title="Date", axis=alt.Axis(format='%d-%m-%Y', labelAngle=-45)),
         y=alt.Y("Résultat:Q", title="Résultat du test"),
@@ -417,12 +431,12 @@ else:
 # --- Bar charts pourcentage ---
 _df_bool = df.copy()
 _df_bool["Date"] = appliquer_granularite(_df_bool)
-df_counts_date = _df_bool.groupby(["Date","OK"]).size().reset_index(name="counts")
+df_counts_date = _df_bool.groupby(["Date","OUTCOME"]).size().reset_index(name="counts")
 df_counts_date["percentage"] = df_counts_date.groupby("Date")["counts"].transform(lambda x: x/x.sum())
 
 _df_table = df.copy()
-fct = _df_table.groupby(["Table ref","OK"]).size().reset_index(name="counts")
-fct["percentage"] = fct.groupby("Table ref")["counts"].transform(lambda x: x/x.sum())
+fct = _df_table.groupby(["TABLE_NAME","OUTCOME"]).size().reset_index(name="counts")
+fct["percentage"] = fct.groupby("TABLE_NAME")["counts"].transform(lambda x: x/x.sum())
 
 col1, col2 = st.columns(2)
 with col1:
@@ -430,16 +444,16 @@ with col1:
     st.altair_chart(alt.Chart(df_counts_date).mark_bar().encode(
         x=alt.X("Date:T", axis=alt.Axis(format='%d-%m-%Y', labelAngle=-45)),
         y=alt.Y("percentage:Q", axis=alt.Axis(format='%')),
-        color=alt.Color("OK:N", scale=alt.Scale(domain=[True,False], range=["#2ca02c","#d62728"])),
-        tooltip=[alt.Tooltip("Date:T", format='%d-%m-%Y'), "OK", alt.Tooltip("percentage:Q", format=".1%")]
+        color=alt.Color("OUTCOME:N", scale=alt.Scale(domain=[True,False], range=["#2ca02c","#d62728"])),
+        tooltip=[alt.Tooltip("Date:T", format='%d-%m-%Y'), "OUTCOME", alt.Tooltip("percentage:Q", format=".1%")]
     ), use_container_width=True)
 with col2:
     st.subheader("Pourcentage de tests OK par Table ref")
     st.altair_chart(alt.Chart(fct).mark_bar().encode(
-        x="Table ref:N",
+        x="TABLE_NAME:N",
         y=alt.Y("percentage:Q", axis=alt.Axis(format='%')),
         color=alt.Color("OK:N", scale=alt.Scale(domain=[True,False], range=["#2ca02c","#d62728"])),
-        tooltip=["Table ref","OK", alt.Tooltip("percentage:Q", format=".1%")]
+        tooltip=["TABLE_NAME","OUTCOME", alt.Tooltip("percentage:Q", format=".1%")]
     ), use_container_width=True)
 
 
@@ -448,15 +462,15 @@ st.subheader("Carte de chaleur : Nombre de tests OK par période et indicateur")
 df_hm = df.copy()
 df_hm["Periode"] = appliquer_granularite(df_hm)
 df_hm["Periode"] = pd.to_datetime(df_hm["Periode"]).dt.strftime('%d-%m-%Y')
-df_ok = df_hm[df_hm["OK"] == True]
+df_ok = df_hm[df_hm["OUTCOME"] == "pass"]
 all_p = sorted(df_hm["Periode"].unique(), key=lambda x: pd.to_datetime(x, format='%d-%m-%Y'))
-df_grp = df_ok.groupby(["Periode","Indicateur de qualité"]).size().reset_index(name="Tests_OK")
-_df_full = pd.DataFrame(index=pd.MultiIndex.from_product([all_p, df_ok["Indicateur de qualité"].unique()], names=["Periode","Indicateur de qualité"]))
-df_full = (_df_full.reset_index().merge(df_grp, on=["Periode","Indicateur de qualité"], how="left").fillna(0))
+df_grp = df_ok.groupby(["Periode","Indicateur"]).size().reset_index(name="Tests_OK")
+_df_full = pd.DataFrame(index=pd.MultiIndex.from_product([all_p, df_ok["Indicateur"].unique()], names=["Periode","Indicateur"]))
+df_full = (_df_full.reset_index().merge(df_grp, on=["Periode","Indicateur"], how="left").fillna(0))
 df_full["Tests_OK"] = df_full["Tests_OK"].astype(int)
 st.altair_chart(alt.Chart(df_full).mark_rect().encode(
     x=alt.X("Periode:O", title="Période", sort=all_p),
-    y=alt.Y("Indicateur de qualité:N", title="Indicateur"),
+    y=alt.Y("Indicateur:N", title="Indicateur"),
     color=alt.Color("Tests_OK:Q", scale=alt.Scale(scheme="greenblue")),
-    tooltip=["Periode","Indicateur de qualité","Tests_OK"]
+    tooltip=["Periode","Indicateur","Tests_OK"]
 ), use_container_width=True)
